871afbdf62b500714237e1e26375ad4aa0a75f84
Mats hjemme <matsgunnerod@gmail.com>
2017-03-06 18:21:47 +0100

    not sure what the changes are :)))

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,16 @@
 module Main exposing (..)
+{-| The main entry of application
+
+# Definition
+@docs main
+
+-}
 
 import App exposing (..)
 import Html exposing (programWithFlags)
 
-
+{-|
+-}
 main : Program String Model Msg
 main =
     programWithFlags { view = view, init = init, update = update, subscriptions = subscriptions }
